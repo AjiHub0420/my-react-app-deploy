8b99564b0d6efa5ce09e2f4b58ec8939cb02cfdf
Aji Nugroho <nugroho0420@gmail.com>
2023-06-29 06:18:11 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import './assets/tablet-responsive.css'
+import './assets/desktop-responsive.css'
+import { ContactMe } from './components/contactme'
+import { Header } from './components/header'
+import { Intro } from './components/intro'
+import { SkillList } from './components/skill'
+import { Footer } from './components/footer'
+// import { ContactMe } from './components/contactme'
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Intro />
+      <SkillList />
+      <ContactMe />
+      <Footer />
+    </>
+  )
 }
 
 export default App;
